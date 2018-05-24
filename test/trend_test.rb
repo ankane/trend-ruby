3b7b5048967fb8ef008855221c04e17e152315b6
Andrew <acekane1@gmail.com>
2018-05-23 23:44:30 -0700

    Test missing and nil values

--- test/trend_test.rb
+++ test/trend_test.rb
@@ -8,6 +8,7 @@ class TrendTest < Minitest::Test
       series[date] = rand(100)
       date += 1
     end
+    series[date - 13] = nil # test nil
     series[date - 8] = 999
 
     assert_equal [Date.parse("2018-04-21")], Trend.anomalies(series)
@@ -20,6 +21,7 @@ class TrendTest < Minitest::Test
       series[date] = date.wday
       date += 1
     end
+    series.delete(date - 13) # test missing
 
     forecast = Trend.forecast(series, count: 7)
     assert_equal [0, 1, 2, 3, 4, 5, 6], forecast.values
